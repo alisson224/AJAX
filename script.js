2eef4cd3304c5165e98745d710a11841f5383381
alisson224 <celularredminote12ss@gmail.com>
2025-09-03 18:22:08 +0000

    quase pronto

---
+++ script.js
@@ -0,0 +1,18 @@
+function buscarUsuario() {
+        const user = document.getElementById('username').value.trim();
+        const result = document.getElementById('result');
+        if (!user) return result.innerHTML = "Digite um usuário!";
+
+     result.innerHTML = "Buscando...";
+        fetch(`https://github.com/users${user}`)
+        .then(res => res.ok ? res.json() : Promise.reject("Usuário não encontrado!"))
+        .then(data => {
+                result.innerHTML = `
+                <img src="${data.avatar_url}" width="80">
+                <p><strong>${data.login}</strong></p>
+                <p>Repositórios: ${data.public_repos}</p>
+                <p>Seguidores: ${data.followers}</p>
+                <a href="${data.html_url}" target="_blank">Perfil</a>`;
+ })
+        .catch(err => result.innerHTML = `<p style="color:red;">${err}</p>`);
+                                                                                  }
